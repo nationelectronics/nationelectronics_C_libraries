57dfc4bc0b15fe3f5431c5a3b36a6d427f3603e9
Nation Electronics <viglesias@nationelectronics.com>
2017-01-28 10:33:35 -0500

    Add files via upload

---
+++ 8Channel-ADC/speedtest.c
@@ -0,0 +1,58 @@
+/*
+ *  C demo code for:
+ *  Raspberry Pi HAT - 8 Channel ADC V1.1
+ *  Microchip MCP3208 chip
+ *
+ *  compile:
+ *  gcc -lwiringPi -o speedtest speedtest.c
+ *
+ *  run:
+ *  ./speedtest
+ *
+ *  Updated 01/28/2017
+ */
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <math.h>
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+#include <time.h>
+
+  /* channel 0..7 */
+int readADCChannel(chip, channel){
+  unsigned char cntrlStack[3];
+  
+  /* figure 6-1 of MCP3208 datasheet */
+  cntrlStack[0] = 6 + ((channel & 4) >> 2);
+  cntrlStack[1] = (channel & 3) << 6;
+  cntrlStack[2] = 0;
+
+  wiringPiSPIDataRW (chip, cntrlStack, 3);
+
+  return ((cntrlStack[1] & 15) << 8) + cntrlStack[2];
+}
+
+int main(int argc, char **argv){
+  int    chip = 0; // (jumper CE0 on) chip = 0 (default), (jumper CE1 on) chip = 1
+  int    result;
+  int    errno;
+
+  if (wiringPiSPISetup (chip, 32000000) < 0){
+     fprintf (stderr, "SPI Setup failed: %s\n", strerror (errno));
+     exit(1);
+  }
+  clock_t begin = clock();
+  int i=0;
+  while (i<100000) {  //read the channel 0, 100000 times
+     i++;
+     result = readADCChannel( chip, 0);
+  }
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  fprintf(stdout,"\n\ntime: %8.6f seconds\n\n", time_spent); // on RPI3 should be less than a second, so, it read at more than 100ksps
+
+  return;
+}
